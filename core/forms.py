2e4b704762a10e6afd953f1366e0c0720c9ef14c
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-12 19:06:31 +0100

    merger

--- core/forms.py
+++ core/forms.py
@@ -1,8 +1,17 @@
 from django import forms
+from django.forms.widgets import ClearableFileInput
 
 from .models import PDFmodel
 
+# class pdfAcceptForm(forms.Form):
+#     forms_file = forms.FileField(widget=forms.FileInput(attrs={
+#         'class' : 'w-full py-4 px-6 rounded-xl border border-gray-400',
+#         'multiple': True
+#     }))
+
+# class pdfAcceptForm(forms.Form): ...
 class pdfAcceptForm(forms.Form):
     forms_file = forms.FileField(widget=forms.FileInput(attrs={
-        'class' : 'w-full py-4 px-6 rounded-xl border border-gray-400'
+        'class' : 'w-full py-4 px-6 rounded-xl border border-gray-400',
     }))
+
