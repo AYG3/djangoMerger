3f5520301881ef60fb2b6e7f975ce14202b4242d
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-12 18:02:53 +0100

    merger

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 
+from .forms import pdfAcceptForm
 
 def base(request):
-    return render(request, 'core/index.html')
+    form = pdfAcceptForm(request.POST, request.FILES)
+    return render(request, 'core/index.html', {'form': form})
