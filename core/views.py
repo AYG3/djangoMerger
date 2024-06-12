28b5dcf5fa55fd0b13825720d5ed1656e7393341
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-12 19:13:40 +0100

    merger

--- core/views.py
+++ core/views.py
@@ -8,10 +8,12 @@ def base(request):
 
         if form.is_valid():
             file = request.FILES['forms_file']
+            files = request.FILES.getlist('forms_file')[-1]
+        
             if not str(file).endswith('.pdf'):
                 return HttpResponse('File is not a pdf file:  ' + str(file))
             
-            return HttpResponse('Form is valid' + str(file))
+            return HttpResponse('Form is valid' + str(file) + '   Second pdf file:   ' + str(files))
     else:
         form = pdfAcceptForm()
     return render(request, 'core/index.html', {'form': form})
