bd49865684f64086e05abb12606b9c5c25e04024
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-12 18:07:33 +0100

    merger

--- core/forms.py
+++ core/forms.py
@@ -3,6 +3,4 @@ from django import forms
 from .models import PDFmodel
 
 class pdfAcceptForm(forms.ModelForm):
-    class Meta:
-        model: PDFmodel
-        fields = ('pdf',)
+    pdf = forms.FileField()
