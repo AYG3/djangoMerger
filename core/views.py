41511c441b6f264b5a58553709583f4167074f07
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-13 09:10:41 +0100

    merger

--- core/views.py
+++ core/views.py
@@ -2,7 +2,9 @@ from django.shortcuts import render, HttpResponse
 
 from .forms import pdfAcceptForm
 
-def base(request):
+
+
+def uploadPDF(request): 
     files = []
     if request.method == 'POST':
         form = pdfAcceptForm(request.POST, request.FILES)
@@ -10,16 +12,22 @@ def base(request):
         if form.is_valid():
             file = request.FILES['forms_file']
             file_len = str(len(request.FILES.getlist('forms_file')))
-            files = request.FILES.getlist('forms_file')[-1]
+            files = request.FILES.getlist('forms_file')
         
             if not str(file).endswith('.pdf'):
                 return HttpResponse('File is not a pdf file:  ' + str(file))
             
-            # return render(request, 'core/index.html', {'form': form, 'files': files })
-            return HttpResponse('Form is valid' + str(file) + '   Second pdf file:   ' + str(files) + '\n The file lenght is:  ' + file_len)
+            return render(request, 'core/index.html', {'form': form, 'files': files })
+            # return HttpResponse('Form is valid' + str(file) + '   Second pdf file:   ' + str(files) + '\n The file lenght is:  ' + file_len)
     else:
         form = pdfAcceptForm()
-    return render(request, 'core/index.html', {'form': form, 'files': files })
+        return render(request, 'core/index.html', {'form': form, 'files': files })
+
+
+def accept(request):
+
+
+
 
 
 # def base(request):
