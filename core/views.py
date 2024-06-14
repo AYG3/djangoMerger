3683fa9e9d324610499c0fe8153f8b484f1c679d
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-14 13:18:08 +0100

    it merges

--- core/views.py
+++ core/views.py
@@ -36,13 +36,13 @@ def mergePDF(request):
     uploaded_files = request.session.get('uploaded_files', [])
     # Loop through all files in the current directory
     print(end='\n\n\n\n\n\n\n')
-    print('UPLOADED_FILES: ' + str(uploaded_files), end='\n\n\n\n\n\n\n\n\n')
+    print('UPLOADED_FILES: ' + str(uploaded_files[0]), end='\n\n\n\n\n\n\n\n\n')
     
 
-    for file_path in uploaded_files:
+    for file_path in uploaded_files[0]:
         # Open each PDF file and append it to the merger object
         # print('FILE PATH:' + str(file_path), end='\n\n\n\n\n')
-        with open(str(file_path), 'rb') as pdf_file:
+        with open(file_path, 'rb') as pdf_file:
             merger.append(pdf_file)
 
     output_file_path = 'media/uploads/Merged.pdf' #attempt to change to'Merged -  first file name'
