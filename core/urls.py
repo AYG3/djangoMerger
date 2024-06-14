fe68d1a827550bf20fa0884874891bc2d600461e
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-14 13:20:41 +0100

    merger

--- core/urls.py
+++ core/urls.py
@@ -6,5 +6,5 @@ app_name = 'core'
 
 urlpatterns = [
     path('', views.uploadPDF, name='upload_pdf'),
-    path('merge/', views.mergePDF, name='merge')
+    path('', views.mergePDF, name='merge')
 ]
