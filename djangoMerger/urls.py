24710774345b9df9936a8487a7ef6dd24d3909c6
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-16 22:50:26 +0100

    merger

--- djangoMerger/urls.py
+++ djangoMerger/urls.py
@@ -23,5 +23,6 @@ from django.conf import settings
 urlpatterns = [
     path('', include('core.urls')),
     path('admin/', admin.site.urls),
+    
 ]
 # ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
