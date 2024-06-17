8d53bb7589c9fe676b4bd6ec1b599b8611212dfe
Ayoku Gilbert <gilbertayoku3@gmail.com>
2024-06-17 12:22:10 +0100

    merger

--- djangoMerger/settings.py
+++ djangoMerger/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,7 +22,7 @@ STATIC_URL = 'static/'
 
 
 # MEDIA_ROOT is the file system path where files uploaded using FileField will be stored.
-MEDIA_ROOT = BASE_DIR / 'media'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # MEDIA_URL is the URL that will serve the media files stored by MEDIA_ROOT. 
